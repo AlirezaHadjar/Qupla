7a76507c8f10544f0264f4fe5cec15502eeb33ab
Ali Nabavi <arz1379n@gmail.com>
2022-11-26 21:17:44 +0330

    create vocabulary class

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,11 @@
+import lexer.vocabulary;
+import org.antlr.v4.codegen.model.Lexer;
+
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
+
+
+
     }
 }
